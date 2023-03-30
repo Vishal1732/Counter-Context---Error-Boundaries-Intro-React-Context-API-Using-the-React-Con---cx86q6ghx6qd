984e1753819941ef8e2da3bb3482d95c8fbf2609
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-03-29 21:25:05 -0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { CounterStateContext } from '../counterContext';
 import '../styles/App.css';
-import { Loader } from './Loader';
-import { PhotoFrame } from './PhotoFrame';
-
-const fetchPosts = async (id) => {
-    let url = `https://jsonplaceholder.typicode.com/photos/${id}`;
-  return fetch(url);
-}
-
+import { CounterList } from './CounterList';
 const App = () => {
-    const[data,setData]=useState([])
-    const[id,setId]=useState(null)
-    const handleChange=(e)=>{
-        setId(e.target.value)
-    }
-    
-    const loadData = async () => {
-        fetchPosts(id)
-          .then((res) => res.json())
-          .then((jsonData) => {
-            setData(jsonData);
-          })
-      }
-    
-      useEffect(() => {
-        loadData();
-      }, []);
-    
-      useEffect(() => {
-        setData(null)
-        loadData()
-      }, [id])
 
-    return(
-        <div>
-            <span>Id number</span>
-            <input type="number" onChange={handleChange} />
-            {data == null ? ( <Loader/> ) : <PhotoFrame url={data.url} title={data.title}/>}
-        </div>
-    )
+  return (
+    <CounterStateContext>
+      <div id="main">
+        <CounterList />
+      </div>
+    </CounterStateContext>
+
+  )
 }
 
 
